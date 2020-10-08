0158b75d584e1bd33497ba9f8deb63d0c773cb8e
DinarZayahov <42997851+DinarZayahov@users.noreply.github.com>
2020-10-08 16:30:41 +0300

    Add files via upload

---
+++ FeatureExtractor/FeatureExtractor.scala
@@ -0,0 +1,20 @@
+def trainModel(documentDF: DataFrame): Word2VecModel = {
+    var word2vecModel: Word2VecModel = null
+    val name = "Word2VecModel"
+    if (Globals.PRODUCTION) {
+      try {
+        word2vecModel = Word2VecModel.load(name)
+      } catch {
+        case e: Exception => throw new Exception(e + "\nMust pre-train models first, set PRODUCTION = false");
+      }
+    }
+    else {
+      val word2Vec = new Word2Vec()
+        .setInputCol("FilteredSentimentText")
+        .setOutputCol("features")
+        .setVectorSize(50)
+        .setMinCount(5)
+        .setSeed(Globals.SEED)
+      word2vecModel = word2Vec.fit(documentDF)
+      word2vecModel.write.overwrite().save(name)
+    }
